c5d098e13cf4262aa2da5395fc7529ada6b756b3
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2022-02-10 15:02:48 +0200

    Lecture-3 Add Arrows task- 5

--- Tasks/Lecture-3/05_own carousel/index.js
+++ Tasks/Lecture-3/05_own carousel/index.js
@@ -1,10 +1,13 @@
 const SlidesPlugin = (activeSlide = 0) => {
-  let currentSlide = activeSlide;
   const slides = document.querySelectorAll('.slide');
+  const prevBtn = document.querySelector('.btn-prev');
+  const nextBtn = document.querySelector('.btn-next');
 
+  let currentSlide = activeSlide;
+ 
   slides[activeSlide].classList.add('active');
 
-  slides.forEach(slide => {
+  slides.forEach((slide) => {
     slide.addEventListener('click', () => {
       clearActiveClasses(slides);
       slide.classList.add('active');
@@ -13,24 +16,39 @@ const SlidesPlugin = (activeSlide = 0) => {
     });
   });
 
+  const bthControl = (btn, changeValue) => {
+    btn.addEventListener('click', () => {
+      clearInterval(interval);
+      changeSlide(changeValue);
+    });
+  };
+
+  bthControl(nextBtn, 1);
+  bthControl(prevBtn, -1);
+
+
   const clearActiveClasses = (slidesArr) => {
     slidesArr.forEach((item) => {
       item.classList.remove('active');
     });
   };
 
-  const changeSlide = () => {
+  const changeSlide = (value) => {
+    currentSlide += value;
     clearActiveClasses(slides);
 
     if (currentSlide > slides.length - 1) {
       currentSlide = 0;
-    } 
+    }
+
+    if (currentSlide < 0) {
+      currentSlide = slides.length - 1;
+    }
 
     slides[currentSlide].classList.add('active');
-    currentSlide += 1;
   };
 
-  const interval = setInterval(() => changeSlide(), 2000);
+  const interval = setInterval(() => changeSlide(1), 2000);
 };
 
 SlidesPlugin();
