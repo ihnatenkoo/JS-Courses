37b9ebb3c19aa563e4cc591df26f5d80f4dc4802
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2022-02-17 13:17:51 +0200

    Lecture-4 Task-1 Fixed

--- Tasks/Lecture-4/Task-1/src/scripts/main.js
+++ Tasks/Lecture-4/Task-1/src/scripts/main.js
@@ -1,22 +1,3 @@
-// import fetch from 'cross-fetch';
-
-const sortByCategory = (a, b) => {
-  const categoryA = a.category.toLowerCase();
-  const categoryB = b.category.toLowerCase();
-
-  if (categoryA > categoryB) {
-    return 1;
-  }
-
-  if (categoryA < categoryB) {
-    return -1;
-  }
-
-  return 0;
-};
-
-const sortByPrice = (a, b) => b.price - a.price;
-
 export const getData = async (url) => {
   try {
     const response = await fetch(url);
@@ -27,14 +8,16 @@ export const getData = async (url) => {
   } catch (error) {
     console.log(error);
   }
-
 };
+
+const sortByPrice = (a, b) => b.price - a.price;
+
+const sortByCategory = (a, b) => a.category.localeCompare(b.category);
 
 const sortData = async () => {
   const productsList = await getData('https://fakestoreapi.com/products');
 
-  const sortPrice = [...productsList].sort(sortByPrice);
-  const sortPriceAndCategory = sortPrice.sort(sortByCategory);
-
-  return sortPriceAndCategory;
+  return productsList.sort(sortByPrice).sort(sortByCategory);
 };
+
+
